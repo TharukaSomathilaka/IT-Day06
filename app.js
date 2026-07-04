f7e3437fae60a9f73862394310aaf00d7b49f665
TharukaSomathilaka <shaniththaruka@gmail.com>
2026-07-04 16:15:50 +0530

    Final

--- app.js
+++ app.js
@@ -1,1 +1,117 @@
-console.log("Hi");
+// class Customer{
+//     name;
+//     age;
+//     address;
+
+//     constructor(name, age, address){
+//         this.name = name;
+//         this.age = age;
+//         this.address = address;
+//     }
+//     getName(){
+//         return this.name;
+//     }
+//     setName(name){
+//         this.name = name;
+//     }
+// }
+
+// let customer1 = new Customer("Sharadha", 20, "Bangalore");
+// console.log(customer1);
+// console.log(customer1.getName());
+
+// ---------------------------------------------
+
+// let customer2 = {
+//     name: "John",
+//     age: 25,
+//     address: "New York",
+//     salary: 50000,
+//     items: [
+//         {
+//             id: 1,
+//             name: "item1",
+//             price: 100,
+//             variants: [
+//                 {
+//                     id: 1,
+//                     name: "variant1",
+//                     price: 10
+//                 },
+//                 {
+//                     id: 2,
+//                     name: "variant2",
+//                     price: 20
+                    
+//                 }
+//             ]
+//         }, {
+//             id: 2,
+//             name: "item2",
+//             price: 200
+//         }, {
+//             id: 3,
+//             name: "item3",
+//             price: 300
+//         }
+//     ],
+//     father: {
+//         name: "Doe",
+//         age: 50,
+//         address: "New York"
+//     }
+// };
+// console.log(customer2.name);
+// console.log(customer2.age);
+// console.log(customer2.address);
+// console.log(customer2.salary);
+// console.log(customer2.items[2].name);
+// console.log(customer2.father.name);
+// console.log("-----------------------");
+
+// console.log(customer2.items.variants);
+
+
+
+// let heading = document.getElementById("heading");
+
+
+
+// function changeHeading() {
+//     let txtInput = document.getElementById("InputText");
+    
+//     console.log(txtInput.value);
+//     let heading = document.getElementById("heading")
+    
+// }
+
+let customerList = [];
+
+function btnAddCustomerOnAction(){
+    let txtName = document.getElementById("txtName").value;
+    let txtAddress = document.getElementById("txtAddress").value;
+    let txtAge = document.getElementById("txtAge").value;
+    let txtEmail = document.getElementById("txtEmail").value;
+    let txtSalary = document.getElementById("txtSalary").value;
+
+
+    let customer={
+    name:txtName,
+    address:txtAddress,
+    age:txtAge,
+    email:txtEmail,
+    salary:txtSalary
+}
+
+customerList.push(customer);
+
+console.log(customerList);
+
+}
+
+
+
+
+
+
+
